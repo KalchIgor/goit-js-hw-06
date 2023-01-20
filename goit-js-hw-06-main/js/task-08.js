0e989a661e3bd89fd0d3518e3100b5b446929895
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-01-20 17:46:10 +0200

    08-09 js

--- goit-js-hw-06-main/js/task-08.js
+++ goit-js-hw-06-main/js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    // Предотвратить отправку   event.preventDefault()
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены')
+    }
+    const userDetails = { email: email.value, Password: password.value }
+
+    console.log(userDetails)
+    event.currentTarget.reset()
+}
